a5cfdc138bdfd13c58b119d6bdb654a4811e3e51
Tiphaine-Lela <34959659+Tiphaine-Lela@users.noreply.github.com>
2018-01-12 14:00:16 +0100

    Create Matière.java

---
+++ Matière.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+
+public class Matière {
+	String name;
+	ArrayList<Cours> liste_cours;
+	public Matière (String n){
+		name = n;
+		liste_cours = new ArrayList<Cours>();
+		
+		
+	}
+}
